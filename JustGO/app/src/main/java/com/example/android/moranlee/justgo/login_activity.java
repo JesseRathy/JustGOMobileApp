5add068681f484c7ead45127ebebec6408378291
Lee Li (yul040) <moran.li@usask.ca>
2017-09-26 12:15:39 -0600

    Update login_activity.java

--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/login_activity.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/login_activity.java
@@ -4,10 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class login_activity extends AppCompatActivity {
+     Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+        login = (Button)findViewById(login);
+    }
+     
+    private View.OnClickListener login(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(),main_menu_activity.class);
+                startActivity(unit_intent);
+            }
+        };
+    }
+
+    private Activity getItSelf(){
+        return this;
     }
 }
