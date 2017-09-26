6789a2a029a582c6f3aa7a15294ac1a8de7f9fc4
Lee Li (yul040) <moran.li@usask.ca>
2017-09-26 12:17:23 -0600

    Update select_food_option_activity.java

--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/select_food_option_activity.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/select_food_option_activity.java
@@ -5,9 +5,28 @@ import android.os.Bundle;
 
 public class select_food_option_activity extends AppCompatActivity {
 
+     Button select_from_old_food;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_food_opition_activity);
+        select_from_old_food = (Button)findViewById(R.id.select_from_old_food);
+        select_from_old_food.setOnClickListener(select_from_old_food());
     }
+    
+     private View.OnClickListener select_from_old_food(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent unit_intent = new Intent(getItSelf(),select_from_food_database_activity.class);
+                startActivity(unit_intent);
+            }
+        };
+    }
+
+    private Activity getItSelf(){
+        return this;
+    }
+    
 }
