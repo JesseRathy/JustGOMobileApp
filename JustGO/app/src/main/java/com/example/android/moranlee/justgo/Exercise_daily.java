8607965f2b53c24111b5e885b568f9802fc544a5
Lee Li (yul040) <moran.li@usask.ca>
2017-09-26 12:12:28 -0600

    Update Exercise_daily.java

--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/Exercise_daily.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/Exercise_daily.java
@@ -5,4 +5,49 @@ package com.example.android.moranlee.justgo;
  */
 
 public class exercise_daily {
+    private int id;
+    private int exercise_id;
+    private int user_id;
+    private String date;
+    private double duration;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getExercise_id() {
+        return exercise_id;
+    }
+
+    public void setExercise_id(int exercise_id) {
+        this.exercise_id = exercise_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
 }
