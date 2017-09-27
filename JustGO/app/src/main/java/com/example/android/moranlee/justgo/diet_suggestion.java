02af3768254623489a41f678472e823de79824c5
Yu Gu <yug242@mail.usask.ca>
2017-09-26 20:17:19 -0600

    Button listener added, diet suggestion interface added

---
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/diet_suggestion.java
@@ -0,0 +1,16 @@
+package com.example.android.moranlee.justgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by yugu on 2017-09-26.
+ */
+
+public class diet_suggestion extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diet_suggestion);
+    }
+}
