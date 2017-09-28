2abd45089289748cdf87d489ee55637cd5e5e181
Yu Gu <yug242@mail.usask.ca>
2017-09-27 20:39:39 -0600

    main menu adjustment

--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/add_new_body_status_activity.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/add_new_body_status_activity.java
@@ -1,13 +1,20 @@
 package com.example.android.moranlee.justgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class add_new_body_status_activity extends AppCompatActivity {
+
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_body_status_activity);
     }
+
+
 }
