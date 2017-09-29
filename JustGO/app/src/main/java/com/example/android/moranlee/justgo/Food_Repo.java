a8b44d46762c9e9f68c196fadc30291672b45301
Lee Li (yul040) <moran.li@usask.ca>
2017-09-28 22:23:57 -0600

    fix duplicate id bug add scoll textview to print all data

--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/Food_Repo.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/Food_Repo.java
@@ -4,9 +4,12 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by yul04 on 2017/9/23.
@@ -14,6 +17,12 @@ import java.util.HashMap;
 
 public class Food_Repo {
     private String [] meats = {"beef","pork","mutton","chicken"};
+    private String [] vegetables = {"cabbage","eggplant","cucumber","mushroom"};
+    private String [] fruits = {"apple","pear","peach","berry"};
+    private String [] dairys = {"milk","yogurt","ice cream","cream"};
+    private String [] fats = {"canola oil","corn oil","peanut oil","butter"};
+    private String [] grains= {"wheat","rice","barley","oat"};
+
 
     private SQLite_Interface sql;
 
@@ -64,14 +73,13 @@ public class Food_Repo {
         values.put("fat",food.getFat());
         values.put("cholesterol",food.getCholesterol());
         values.put("calories",food.getCalories());
-
         db.update("food", values, "id" + "= ?", new String[] { String.valueOf(food.getId()) });
         db.close();
     }
 
-    private Food cretae_default_foods(int i){
+    private Food cretae_default_meats(int i){
         Food a_food = new Food();
-        a_food.setId(0);
+        a_food.setId(i);
         a_food.setName(meats[i]);
         a_food.setCalories(Math.random());
         a_food.setCategory(1);
@@ -82,23 +90,105 @@ public class Food_Repo {
         return a_food;
     }
 
+    private Food cretae_default_fruits(int i){
+        Food a_food = new Food();
+        a_food.setId(i+meats.length);
+        a_food.setName(fruits[i]);
+        a_food.setCalories(Math.random());
+        a_food.setCategory(2);
+        a_food.setCholesterol(Math.random());
+        a_food.setFat(Math.random());
+        a_food.setUser_id(0);
+        a_food.setProtein(Math.random());
+        return a_food;
+    }
+
+    private Food cretae_default_vegetables(int i){
+        Food a_food = new Food();
+        a_food.setId(i+meats.length+fruits.length);
+        a_food.setName(vegetables[i]);
+        a_food.setCalories(Math.random());
+        a_food.setCategory(3);
+        a_food.setCholesterol(Math.random());
+        a_food.setFat(Math.random());
+        a_food.setUser_id(0);
+        a_food.setProtein(Math.random());
+        return a_food;
+    }
+
+    private Food cretae_default_dairys(int i){
+        Food a_food = new Food();
+        a_food.setId(i+meats.length+fruits.length+vegetables.length);
+        a_food.setName(dairys[i]);
+        a_food.setCalories(Math.random());
+        a_food.setCategory(4);
+        a_food.setCholesterol(Math.random());
+        a_food.setFat(Math.random());
+        a_food.setUser_id(0);
+        a_food.setProtein(Math.random());
+        return a_food;
+    }
+    private Food cretae_default_grains(int i){
+        Food a_food = new Food();
+        a_food.setId(i+meats.length+fruits.length+vegetables.length+dairys.length);
+        a_food.setName(grains[i]);
+        a_food.setCalories(Math.random());
+        a_food.setCategory(5);
+        a_food.setCholesterol(Math.random());
+        a_food.setFat(Math.random());
+        a_food.setUser_id(0);
+        a_food.setProtein(Math.random());
+        return a_food;
+    }
+    private Food cretae_default_fats(int i){
+        Food a_food = new Food();
+        a_food.setId(i+meats.length+fruits.length+vegetables.length+dairys.length+grains.length);
+        a_food.setName(fats[i]);
+        a_food.setCalories(Math.random());
+        a_food.setCategory(6);
+        a_food.setCholesterol(Math.random());
+        a_food.setFat(Math.random());
+        a_food.setUser_id(0);
+        a_food.setProtein(Math.random());
+        return a_food;
+    }
+
     private void add_default_food(){
         for(int i=0;i<4;i++){
-            insert(cretae_default_foods(i));
+            insert(cretae_default_meats(i));
+        }
+        for(int i=0;i<4;i++){
+            insert(cretae_default_fruits(i));
+        }
+        for(int i=0;i<4;i++){
+            insert(cretae_default_vegetables(i));
+        }
+        for(int i=0;i<4;i++){
+            insert(cretae_default_dairys(i));
+        }
+        for(int i=0;i<4;i++){
+            insert(cretae_default_grains(i));
+        }
+        for(int i=0;i<4;i++){
+            insert(cretae_default_fats(i));
         }
     }
 
     public ArrayList<HashMap<String, String>>  get_default_food_list() {
-
         SQLiteDatabase db = sql.getReadableDatabase();
         String selectQuery =  "select * from food where user_id = 0";
         ArrayList<HashMap<String, String>> foodList = new ArrayList<HashMap<String, String>>();
+        Log.d(TAG, "get_default_food_list: "+db.toString());
         Cursor cursor = db.rawQuery(selectQuery, null);
         if (cursor.moveToFirst()) {
             do {
                 HashMap<String, String> food = new HashMap<String, String>();
                 food.put("id", cursor.getString(cursor.getColumnIndex("id")));
                 food.put("name", cursor.getString(cursor.getColumnIndex("name")));
+                food.put("protein",cursor.getString(cursor.getColumnIndex("protein")));
+                food.put("fat",cursor.getString(cursor.getColumnIndex("protein")));
+                food.put("calories",cursor.getString(cursor.getColumnIndex("calories")));
+                food.put("cholesterol",cursor.getString(cursor.getColumnIndex("cholesterol")));
                 foodList.add(food);
 
             } while (cursor.moveToNext());
--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/select_from_food_database_activity.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/select_from_food_database_activity.java
@@ -2,6 +2,7 @@ package com.example.android.moranlee.justgo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ public class select_from_food_database_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_from_food_database_activity);
         show_from_database = (TextView)findViewById(R.id.show_from_database);
+        show_from_database.setMovementMethod(new ScrollingMovementMethod());
         getFoods = new Food_Repo(this);
         String text = "";
         ArrayList defaults = getFoods.get_default_food_list();
