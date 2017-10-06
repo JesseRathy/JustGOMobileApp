b1d43a591697633a67d64a37e12e9c80a1f2b8bf
Yu Gu <yug242@mail.usask.ca>
2017-10-06 11:39:19 -0600

    drawable files and layout update for expandablelistview

---
+++ JustGO/app/src/main/java/adapter/OnGroupExpandedListener.java
@@ -0,0 +1,14 @@
+package adapter;
+
+/**
+ * Created by yugu on 2017-10-05.
+ */
+
+public interface OnGroupExpandedListener {
+    /**
+     * 分组展开
+     *
+     * @param groupPosition
+     */
+    void onGroupExpanded(int groupPosition);
+}
---
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/activity/Constant.java
@@ -0,0 +1,19 @@
+package com.example.android.moranlee.justgo.activity;
+
+/**
+ * @author yugu on 2017.07.31
+ *         constant class
+ */
+public class Constant {
+    public static final String[] BOOKS = {"b1", "b2", "b3", "b4"};
+    public static final String[][] FIGURES = {
+            {"f1", "f2", "f3", "f4"},
+            {"a1", "a2", "a3", "a4"},
+            {"b1", "b2", "b3", "b4", "b5"},
+            {"c1", "c2", "c3", "c4"}
+    };
+
+    public static final String BOOK_NAME = "book_name";
+    public static final String FIGURE_NAME = "figure_name";
+
+}
