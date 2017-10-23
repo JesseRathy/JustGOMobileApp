5f0bfb0f65fdcf40fdd777ed1cc2f0590e8dd43e
Jesse <jrr867@mail.usask.ca>
2017-10-23 17:35:22 -0600

    Added working code for changing password; also actually added the button listener to the submit button in profile change button; the actual change profile code needs to be changed to work better, however.

--- JustGO/app/src/main/java/com/example/android/moranlee/justgo/activity/activity/edit_profile_activity.java
+++ JustGO/app/src/main/java/com/example/android/moranlee/justgo/activity/activity/edit_profile_activity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,6 +46,7 @@ public class edit_profile_activity extends AppCompatActivity {
         passwordBtn = (Button) findViewById(R.id.profile_change_password);
         user_repo = new User_Repo(this);
         passwordBtn.setOnClickListener(changePassword());
+        submit.setOnClickListener(submit_change());
         }
 
 
@@ -53,6 +55,7 @@ public class edit_profile_activity extends AppCompatActivity {
           @Override
             public void onClick(View v) {
                Intent unit_intent = new Intent(getItSelf(), change_password_activity.class);
+
                startActivity(unit_intent);
           }
        };
